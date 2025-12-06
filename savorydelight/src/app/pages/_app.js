4908497d305d46c37c9176b310a8c3a72650f1d9
Udeshi Edirisinghe <ar112949@fhss.sjp.ac.lk>
2025-12-06 14:35:56 +0530

    Installed necessary packages and deleted page folder

--- savorydelight/src/app/pages/_app.js
+++
@@ -1,9 +0,0 @@
-// src/pages/_app.js
-
-// 🛑 Styles මෙතැනින් අනිවාර්යයෙන්ම Import කළ යුතුය
-import '../styles/globals.css'; 
-
-export default function MyApp({ Component, pageProps }) {
-  // ඔබගේ Pages Router පිටු සියල්ලටම Global CSS ලැබේ.
-  return <Component {...pageProps} />;
-}
