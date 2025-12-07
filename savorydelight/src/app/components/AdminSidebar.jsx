3e135869a0e462c4274b6908f6d5814767dc5233
Keshara Abeywickrama <ar112936@fhss.sjp.ac.lk>
2025-12-07 20:01:55 +0530

    changed admin sidemenubar

--- savorydelight/src/app/components/AdminSidebar.jsx
+++ savorydelight/src/app/components/AdminSidebar.jsx
@@ -47,11 +47,12 @@ export default function AdminSidebar() {
 
       {/* Logout */}
       <div className="mt-auto">
-        <button className="flex items-center gap-3 bg-white rounded-lg px-4 py-2">
+         <button className="flex items-center gap-3 bg-white rounded-lg px-4 py-2 text-black font-semibold hover:bg-gray-200">
           <FaSignOutAlt />
-          Logout
-        </button>
+            Logout
+         </button>
       </div>
+
     </aside>
   );
 }
